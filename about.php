fdfa28c7d8a11cbea0bdbed5f7207b3f05ad0c4a
unknown <bashamibrahim68@gmail.com>
2019-11-11 18:01:21 +0530

    updated with about page

--- about.php
+++ about.php
@@ -8,6 +8,7 @@
     <script src="https://kit.fontawesome.com/957812a91c.js"></script>
     <link rel="stylesheet" href="style\styles.css">
     <link rel="stylesheet" href="style\navbar.css">
+    <link rel="stylesheet" href="style\about.css">
     <title>ALTA-products of the future</title>
 </head>
 <body>
@@ -22,8 +23,43 @@
               <li><a href="" class="selected btn-glow"><span></span><span></span><span></span><span></span>about</a></li>
               <li><a href="contact.php" class="btn-glow"><span></span><span></span><span></span><span></span>contact</a></li>
 
-          </ul>
-                </div>
-            </div>  
+            </ul>
+              </div>
+        </div>  
+     <div class="Parallax">
+     <ul class="par">
+     <li><a href="#Story">Story</a></li>
+     <li><a href="#Co-branding">Co-branding</li>
+     <li><a href="#Events">Events</a></li>
+     <li><a href="#News">News</a></li>
+     <li><a href="#Activities">Activities</a></li>
+     </ul>
+</div>
+<div class="story-area" id="Story">
+    <div class="text-part">
+      <h1>Story</h1>
+</div>
+
+</div>
+<div class="CoBranding-area" id="Co-branding">
+<div class="text-part">
+      <h1>Co-branding</h1>
+</div>
+</div>
+<div class="Events-area" id="Events">
+<div class="text-part">
+      <h1>Events</h1>
+</div>
+</div>
+<div class="news-area" id="News">
+<div class="text-part">
+      <h1>News</h1>
+</div>
+</div>
+<div class="activities-area" id="Activities">
+<div class="text-part">
+      <h1>Activities</h1>
+</div>
+</div>
 </body>
 </html>
---
+++ story.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/957812a91c.js"></script>
+     <link rel="stylesheet" href="style\styles.css">
+    <link rel="stylesheet" href="style\navbar.css">
+    <link rel="stylesheet" href="style\story.css">
+    <title>ALTA-products of the future</title>
+</head>
+<body>
+    <div class="container"> 
+        <div class="box">
+            <div class="imgBx">
+</div>
+            <div class="content">
+</div>
+</div>
+
+</body>
+</html>
